1be9485bf5fed902ae2265e3a464245965a5e53e
pavel.todorov <pavel.todorov@hiq.ru>
2021-02-04 13:26:41 +0300

    Add simple form

--- src/index.js
+++ src/index.js
@@ -3,31 +3,48 @@ import './js/bootstrap.min';
 import './css/bootstrap.min.css';
 
 function component() {
-  const label = document.createElement('label');
-  label.setAttribute('for', 'exampleRSS');
+  // RSS link
+  // Example: https://ru.hexlet.io/lessons.rss
+
+  const baseHeader = document.createElement('h1');
+  baseHeader.textContent = 'RSS Reader';
+
+  const tagLine = document.createElement('summary');
+  tagLine.textContent = 'Start reading RSS today! It is easy, it is nicely.';
+
+  const label = document.createElement('span');
+  label.classList.add('input-group-text');
   label.textContent = 'RSS link:';
 
   const input = document.createElement('input');
-  input.type = 'rss';
+  input.type = 'text';
   input.classList.add('form-control');
   input.id = 'cssLink';
 
   const formGroup = document.createElement('div');
-  formGroup.classList.add('form-group');
+  formGroup.classList.add('input-group', 'mb-3');
   formGroup.appendChild(label);
   formGroup.appendChild(input);
 
   const button = document.createElement('button');
   button.type = 'submit';
   button.classList.add('btn', 'btn-primary');
-  button.textContent = 'Submit';
+  button.textContent = 'Add';
 
   const form = document.createElement('form');
+  form.classList.add('container');
   form.appendChild(formGroup);
   form.appendChild(button);
 
+  const formContaner = document.createElement('div');
+  formContaner.classList.add('mt-5');
+  formContaner.appendChild(form);
+
   const main = document.createElement('div');
-  main.appendChild(form);
+  main.classList.add('container');
+  main.appendChild(baseHeader);
+  main.appendChild(tagLine);
+  main.appendChild(formContaner);
 
   return main;
 }
