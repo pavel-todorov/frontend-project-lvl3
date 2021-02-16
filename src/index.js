f4e65a56fe6d46c9500c9bebe82ab102a5f47ac5
pavel.todorov <pavel.todorov@hiq.ru>
2021-02-16 12:16:39 +0300

    add init for main.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import './libs/bootstrap/js/bootstrap';
 import './libs/bootstrap/css/bootstrap.css';
-import {
-  generateMainPage,
-  subscribeOnViewEvents,
-} from './mainPage/mainPageView';
-import initMainPageModel from './mainPage/mainPageModel';
-import {
-  mainPageModelChangeCallback,
-  setMainPageModel,
-  mainPageViewEvents,
-} from './mainPage/mainPageController';
-import { initTranslations } from './utils/translations/translations';
+// import {
+//   generateMainPage,
+//   subscribeOnViewEvents,
+// } from './mainPage/mainPageView';
+// import initMainPageModel from './mainPage/mainPageModel';
+// import {
+//   mainPageModelChangeCallback,
+//   setMainPageModel,
+//   mainPageViewEvents,
+// } from './mainPage/mainPageController';
+// import { initTranslations } from './utils/translations/translations';
 import 'webpack-icons-installer/bootstrap';
+import init from './main'
 
-initTranslations().then(() => {
-  document.body.appendChild(generateMainPage());
-  setMainPageModel(initMainPageModel(mainPageModelChangeCallback));
-  subscribeOnViewEvents(mainPageViewEvents);
-});
+init().then(() => {});
+
+// initTranslations().then(() => {
+//   document.body.appendChild(generateMainPage());
+//   setMainPageModel(initMainPageModel(mainPageModelChangeCallback));
+//   subscribeOnViewEvents(mainPageViewEvents);
+// });
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import {
+  generateMainPage,
+  subscribeOnViewEvents,
+} from './mainPage/mainPageView';
+import initMainPageModel from './mainPage/mainPageModel';
+import {
+  mainPageModelChangeCallback,
+  setMainPageModel,
+  mainPageViewEvents,
+} from './mainPage/mainPageController';
+import { initTranslations } from './utils/translations/translations';
+  
+const init = async () => {
+  initTranslations().then(() => {
+    document.body.appendChild(generateMainPage());
+    setMainPageModel(initMainPageModel(mainPageModelChangeCallback));
+    subscribeOnViewEvents(mainPageViewEvents);  
+  });
+};
+
+export default init;
