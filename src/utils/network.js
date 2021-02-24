8edc9bc332f9c1e5eb85dc75ecf2a6bd41345cb4
pavel.todorov <pavel.todorov@hiq.ru>
2021-02-24 09:19:20 +0300

    Fix network parameter name

--- src/utils/network.js
+++ src/utils/network.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const download = (link) => {
   return axios
     // .get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(link)}&disabledCache=true&timestamp=${new Date().getTime()}`);
-    .get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(link)}&disabledCache=true`);
+    .get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(link)}&disableCache=true`);
 };
 
 module.exports = {
